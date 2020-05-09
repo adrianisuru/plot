96458a5bd6524a74f39e747fd4f5b1eae68f6da9
Adrian Herath <adrianisuru@gmail.com>
2020-05-09 03:55:29 -0500

    Draw line loop

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,26 @@
 use wasm_bindgen::prelude::*;
 use wasm_bindgen::JsCast;
-use web_sys::{WebGlProgram, WebGlRenderingContext, WebGlShader};
+use web_sys::{Window, WebGlProgram, WebGlRenderingContext, WebGlShader};
+use web_sys::console;
+
+
+#[wasm_bindgen]
+extern "C" {
+    // Use `js_namespace` here to bind `console.log(..)` instead of just
+    // `log(..)`
+    #[wasm_bindgen(js_namespace = console)]
+    fn log(s: &str);
+
+    // The `console.log` is quite polymorphic, so we can bind it with multiple
+    // signatures. Note that we need to use `js_name` to ensure we always call
+    // `log` in JS.
+    #[wasm_bindgen(js_namespace = console, js_name = log)]
+    fn log_u32(a: u32);
+
+    // Multiple arguments too!
+    #[wasm_bindgen(js_namespace = console, js_name = log)]
+    fn log_many(a: &str, b: &str);
+}
 
 #[wasm_bindgen(start)]
 pub fn start() -> Result<(), JsValue> {
@@ -8,23 +28,23 @@ pub fn start() -> Result<(), JsValue> {
     let canvas = document.get_element_by_id("canvas").unwrap();
     let canvas: web_sys::HtmlCanvasElement = canvas.dyn_into::<web_sys::HtmlCanvasElement>()?;
 
-    let context = canvas
+    let gl = canvas
         .get_context("webgl")?
         .unwrap()
         .dyn_into::<WebGlRenderingContext>()?;
 
     let vert_shader = compile_shader(
-        &context,
+        &gl,
         WebGlRenderingContext::VERTEX_SHADER,
         r#"
-        attribute vec4 position;
+        attribute vec3 position;
         void main() {
-            gl_Position = position;
+            gl_Position = vec4(position, 1.0);
         }
     "#,
     )?;
     let frag_shader = compile_shader(
-        &context,
+        &gl,
         WebGlRenderingContext::FRAGMENT_SHADER,
         r#"
         void main() {
@@ -32,13 +52,54 @@ pub fn start() -> Result<(), JsValue> {
         }
     "#,
     )?;
-    let program = link_program(&context, &vert_shader, &frag_shader)?;
-    context.use_program(Some(&program));
+    let program = link_program(&gl, &vert_shader, &frag_shader)?;
+    gl.use_program(Some(&program));
 
-    let vertices: [f32; 9] = [-0.7, -0.7, 0.0, 0.7, -0.7, 0.0, 0.0, 0.7, 0.0];
+    const size: usize = 20;
+    let mut v: [(f32, f32, f32, f32); size * size] = [(0.0, 0.0, 0.0, 0.0); size * size];
+    for i in 0..size * size {
+        let (x, y) = (i % size, i / size);
+        let (x, y) = (x as f32 / (size - 1) as f32, y as f32 / (size - 1) as f32);
+        v[i] = (-1.0 + 2.0 * x, 1.0 - 2.0 * y, 0.0, 0.0);
+    }
+    let v = v;
+    let mut vertices: [f32; 3 * size * size] = [0.0; 3 * size * size];
+    for i in 0..size * size {
+        vertices[3 * i + 0] = v[i].0;
+        vertices[3 * i + 1] = v[i].1;
+        vertices[3 * i + 2] = v[i].2;
+    }
+    let vertices = vertices;
 
-    let buffer = context.create_buffer().ok_or("failed to create buffer")?;
-    context.bind_buffer(WebGlRenderingContext::ARRAY_BUFFER, Some(&buffer));
+
+    for (x, y, _, _) in v.iter() {
+        println!("({},{})", x, y);
+    }
+
+
+    let mut indices = [0u16; 1 * size * size];
+    {
+        let mut k = 0;
+        for i in 0..size {
+            for j in 0..size {
+                let (x, y) = (i, if i % 2 == 0 {j} else {size - j - 1});
+                indices[k] = (x * size + y) as u16;
+                //indices[k] = k as u16;
+                log_u32(indices[k] as u32);
+                k = k + 1;
+            }
+        }
+    }
+
+
+
+
+    let indices = indices;
+
+
+    let buffer = gl.create_buffer().ok_or("failed to create buffer")?;
+    gl.bind_buffer(WebGlRenderingContext::ARRAY_BUFFER, Some(&buffer));
+
 
     // Note that `Float32Array::view` is somewhat dangerous (hence the
     // `unsafe`!). This is creating a raw view into our module's
@@ -51,25 +112,41 @@ pub fn start() -> Result<(), JsValue> {
     unsafe {
         let vert_array = js_sys::Float32Array::view(&vertices);
 
-        context.buffer_data_with_array_buffer_view(
+        gl.buffer_data_with_array_buffer_view(
             WebGlRenderingContext::ARRAY_BUFFER,
             &vert_array,
             WebGlRenderingContext::STATIC_DRAW,
         );
     }
 
-    context.vertex_attrib_pointer_with_i32(0, 3, WebGlRenderingContext::FLOAT, false, 0, 0);
-    context.enable_vertex_attrib_array(0);
+    let buffer = gl.create_buffer().ok_or("failed to create buffer")?;
+    gl.bind_buffer(WebGlRenderingContext::ELEMENT_ARRAY_BUFFER, Some(&buffer));
 
-    context.clear_color(0.0, 0.0, 0.0, 1.0);
-    context.clear(WebGlRenderingContext::COLOR_BUFFER_BIT);
+    unsafe {
+        let indices_array = js_sys::Uint16Array::view(&indices);
 
-    context.draw_arrays(
-        WebGlRenderingContext::TRIANGLES,
-        0,
-        (vertices.len() / 3) as i32,
-    );
+        gl.buffer_data_with_array_buffer_view(
+            WebGlRenderingContext::ELEMENT_ARRAY_BUFFER,
+            &indices_array,
+            WebGlRenderingContext::STATIC_DRAW,
+        );
+    }
+
+    gl.enable_vertex_attrib_array(0);
+    gl.vertex_attrib_pointer_with_i32(0, 3, WebGlRenderingContext::FLOAT, false, 0, 0);
+
+    gl.clear_color(0.0, 0.0, 1.0, 1.0);
+    gl.clear(WebGlRenderingContext::COLOR_BUFFER_BIT);
+
+    gl.draw_elements_with_i32(
+        WebGlRenderingContext::LINE_LOOP,
+        indices.len() as i32,
+        WebGlRenderingContext::UNSIGNED_SHORT,
+        0);
     Ok(())
+}
+
+pub fn gen_vertices(size: u16, vertices: &mut [(f32, f32)]) -> () {
 }
 
 pub fn compile_shader(
